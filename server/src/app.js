67b1bfab2bf80f50d4ec3cacff5120c2c14a9c8e
Josh Beasley <josh.beasley@hotmail.com>
2022-12-05 09:44:14 -0800

    added delete functionality

--- server/src/app.js
+++ server/src/app.js
@@ -34,6 +34,17 @@ app.post('/movies', async (req, res) => {
   }
 })
 
+app.delete('/movies/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try{
+    await knex('movie').where('id', id).del();
+    res.status(202).send(`Movie with id ${id} successfully deleted.`);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
