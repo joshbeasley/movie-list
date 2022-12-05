19b76f1eb70253a1b3d7dae958143b65efeab51b
Josh Beasley <josh.beasley@hotmail.com>
2022-12-05 09:27:37 -0800

    added search functionality for the movie list

--- frontend/src/components/MovieList.jsx
+++ frontend/src/components/MovieList.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { MovieItem } from './MovieItem';
 import Grid from '@mui/material/Grid';
 import { Box } from '@mui/material';
+import { SearchBar } from './SearchBar';
 
 export const MovieList = () => {
-  const movies = [
-    {title: 'Mean Girls'},
-    {title: 'Hackers'},
-    {title: 'The Grey'},
-    {title: 'Sunshine'},
-    {title: 'Ex Machina'},
-  ];
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/movies');
+        const data = await response.json();
+        setMovies(data);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+    getMovies();
+  }, []);
+
+  useEffect(() => {
+    
+  }, [search]);
 
   return (
     <Box sx={{marginTop: '20px', paddingX: '20px'}}>
+      <SearchBar val={search} handleSearchChange={e => setSearch(e.target.value)}/>
       <Grid container  spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} >
-        {movies.map((movie, index) => (
+        {movies.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase())).map((movie, index) => (
           <Grid item xs={2} sm={4} md={4} key={index}>
-            <MovieItem key={index} title={movie.title}/>
+            <MovieItem key={movie.id} title={movie.title}/>
           </Grid>
         ))}
       </Grid>
--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const knex = require('knex')(require('../knexfile.js')["development"]);
+const cors = require('cors');
 
 const app = express();
 const port = 8080;
 
+app.use(cors())
 app.use(express.json());
 
 app.get('/movies', async (req, res) => {
